5e3057365761f0d06260e94199421d15771d5e71
Sanjay Bhatt <sanjay.bhatt0621@gmail.com>
2025-06-19 22:42:14 +0530

    NodeLearning | Diffrent HTTP methods in routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,28 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send("User data fetch successfully");
+});
+
+app.post("/user", (req, res) => {
+  // Data saved to DB in this step
+  res.send("User data posted successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("User data modified successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User data deleted successfully");
+});
+
 app.use("/dashboard", (req, res) => {
   res.send("Hello from dashboard!");
 });
 
-app.use((req, res) => {
+app.use("/", (req, res) => {
   res.send("Hello from server end!");
 });
 app.listen("1919", () => {
